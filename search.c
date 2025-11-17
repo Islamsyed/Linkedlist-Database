7070b63670c990dd9404dbafe837ae694530414b
Islamsyed <66957756+Islamsyed@users.noreply.github.com>
2025-11-17 22:37:24 +0530

    Implement search_record function for linked list

---
+++ search.c
@@ -0,0 +1,25 @@
+void search_record(NODE *ptr, int key)
+{
+    NODE *temp = ptr;
+
+    if(temp == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    while(temp != NULL)
+    {
+        if(temp->roll == key)
+        {
+            printf("Record Found:\n");
+            printf("Roll  : %d\n", temp->roll);
+            printf("Name  : %s\n", temp->name);
+            printf("Marks : %.2f\n", temp->marks);
+            return;
+        }
+        temp = temp->link;
+    }
+
+    printf("Record not found\n");
+}
